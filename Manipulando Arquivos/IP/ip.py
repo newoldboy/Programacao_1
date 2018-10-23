17a83ba59b6d00924578967ad8c177a7cf5ce7b7
newoldboy <guilherme.pereira1609@gmail.com>
2018-10-22 22:13:59 -0300

    .

---
+++ Manipulando Arquivos/IP/ip.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+
+ips = []
+contador = int(input('Insira quantos ips você deseja adicionar: '))              
+while len(ips) < contador:         
+    ips.append(input('Insira seu ip: '))
+for ips_valores in ips:
+    addr = ips_valores
+    try:
+        socket.inet_aton(addr)
+        print("IP Valido")
+        arq = open('ips.txt', 'a+')
+        texto = []
+        texto.append(str(ips))
+        arq.writelines(texto)
+        arq.close()
+    except socket.error:
+        print("IP Invalido")
+        raise 
