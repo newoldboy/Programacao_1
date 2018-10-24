9257b47d3adeca127ddbacadfb0d48b2698f1228
Guilherme <guilherme.pereira1609@gmail.com>
2018-10-23 23:25:34 -0200

    .

--- Manipulando Arquivos/countPalavras/ContaPalavra.py
+++ Manipulando Arquivos/countPalavras/ContaPalavra.py
@@ -1,33 +1,20 @@
-arquivo = open('count.txt', 'r')
+#  LOGICA PARA PEGAR CADA PALAVRA NÃO FUNCIONA AINDA
 count = 0
-palavra = input('''
-    Digite a palavra que deseja verificar sua existencia no texto: ''')
+lista = []
+palavra = ''
+arquivo = open('count.txt', 'r')
 for linha in arquivo:
     linha = linha.rstrip()
-    if palavra in linha:
-        count = count + 1
-        print(palavra + ' está contida em ' + linha)      
-print('Tem: ', count)
+    palavra = linha.split(" ")
+    lista += palavra
+# print(lista)
 arquivo.close()
 
+for palavra in lista:
+    count = 0
+    for palavra2 in lista:
+        if palavra == palavra2:
+            count += 1
+    print('"' + palavra + '"' + ' Repetiu: ', count,'\n')
 
-#  LOGICA PARA PEGAR CADA PALAVRA NÃO FUNCIONA AINDA
-# count = 0
-# pula = 0
-# arquivo = open('count.txt', 'r')
-# for linha in arquivo:
-#     arquivo = open('count.txt', 'r')
-#     text = arquivo.readline()
-#     palavra = text.split(" ")[pula]
-#     arquivo.close()
-#     print('''
-#     palavra é
-#     = ''' + palavra)
-#     linha = linha.rstrip()
-#     for palavra in linha:  
-#         if palavra in linha:
-#             count = count + 1
-#             pula = pula + 1
-#             print(linha)       
-# print('Tem: ', count)
-# arquivo.close()
+  
